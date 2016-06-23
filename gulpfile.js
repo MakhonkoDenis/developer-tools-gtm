63323f0eb3fee78b82585c9ab80ffc64325be993
MakhonkoDenis <makhonkodenis@gmail.com>
2016-06-24 00:46:32 +0300

    UPD: change structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,90 +2,93 @@
 
 var path = {
 		base: __dirname + '/',
-		assets: {
-			js: '**/*.js',
-			ignorJs: '**/*.min.js',
-
-			scss: '**/*.scss',
-
-			css: '**/*.css',
-			ignorCss: '**/*.min.css',
-
-			img: '**/*.+(jpg|jpeg|png|svg|gif)',
+		js: {
+			input: [
+				'./modules/**/*.js',
+				'!**/*.min.js',
+				'!./node_modules',
+				'!./gulpfile.js',
+			],
+			output:{
+				path:'min/',
+				sufix: '.min'
+			}
 		},
-		cherryFramework: {
-			base: 'cherry-framework/',
-			modules: 'modules/'
-		}
+		scss: {
+			input: [
+				'**/*.scss',
+			],
+			output:{
+				path:'min/',
+				sufix: '.min'
+			}
+		},
+		css: {
+			input: [
+				'**/*.css',
+				'!**/*.min.css',
+			],
+			output:{
+				path:'min/',
+				sufix: '.min'
+			}
+		},
+		img: [
+			'**/*.+(jpg|jpeg|png|svg|gif)',
+		],
+		logs: 'logs/'
 	},
-	task = [
-		'js',
-		'scss',
-		/*'css',*/
-	],
-	useFiles = [
-		path.base + path.cherryFramework.modules + path.assets.js,
-		'!' + path.base + path.cherryFramework.modules + path.assets.ignorJs,
-
-		path.base + path.cherryFramework.modules + path.assets.scss,
-
-		//path.base + path.cherryFramework.modules + path.assets.css,
-		//'!' + path.base + path.cherryFramework.modules + path.assets.ignorCss,
-	],
+	task = {
+		default: 'default',
+		watch: 'watch',
+		compile: [
+			'compile:js', //compile and compress file
+			'compile:scss', //compile and compress file
+			'compile:css', //compress and concat file
+		],
+		check: [
+			'check:js',
+			'check:php',
+		],
+		compress: [
+			'compress:img',
+		],
+	},
 	gulp = require( 'gulp' ),
-	rename = require( 'gulp-rename' ),
-	uglify = require( 'gulp-uglify' );
+	rename = require( 'gulp-rename' );
 
 
 gulp
-	.task( 'default', task )
-	.task( 'watch', watchHandler )
-	.task( 'js', js )
-	.task( 'sass', scss )
-	.task( 'css', css )
-	.task( 'img', compressImg );
+	//.task( task.default, task )
+	//.task( task.watch, watchHandler )
+	.task( task.compile[0], compileJs )
+	.task( task.check[0], checkJs )
+/*	.task( compile[1], compileCcss )
+	.task( compile[2], compileCss )
+	.task( compress[0], compressImg )*/;
 
-function watchHandler() {
-	var watcher = gulp.watch(
-		[
-			path.base + path.cherryFramework.modules + path.assets.js,
-			'!' + path.base + path.cherryFramework.modules + path.assets.ignorJs,
-			path.base + path.cherryFramework.modules + path.assets.scss,
-		],
-		task
-	);
-
-	watcher
-		.on( 'change', function( event ) {
-			console.log( 'Event type: ' + event.type );
-			console.log( 'Event path: ' + event.path );
-		} )
-		.on( 'error', function( event ) {
-			console.log( 'Event type: ' + event.type );
-			console.log( 'Event path: ' + event.path );
-		} );
-}
-
-function js(){
-	var jshint = require( 'gulp-jshint' ),
-		uglify = require( 'gulp-uglify' ),/*
-		stylish = require('gulp-jshint-file-reporter'),*/
-		concat = require( 'gulp-concat' );
+function compileJs(){
+	var uglify = require( 'gulp-uglify' )/*,
+		stylish = require('gulp-jshint-file-reporter'),*/;
 
 	gulp
-		.src( [
-			path.base + path.cherryFramework.modules + path.assets.js,
-			'!' + path.base + path.cherryFramework.modules + path.assets.ignorJs,
-		] )
-		.pipe( jshint() )
-		.pipe( jshint.reporter( 'gulp-jshint-file-reporter', {
-			filename: './logs/jshint-output.log'
-		} ) )
+		.src( path.js.input )
 		.pipe( uglify() )
 		.pipe( rename( function( path ){
-			renameFile( path, '/min/', '.min')
+			renameFile( path, '/min/', '.min' )
 		} ) )
 		.pipe( gulp.dest( path.base + path.cherryFramework.modules ) );
+};
+function checkJs(){
+	var jshint = require( 'gulp-jshint' )/*,
+		concat = require( 'gulp-concat' )*/;
+
+	gulp
+		.src( path.js.input )
+		.pipe( jshint() )
+		.pipe( jshint.reporter( 'gulp-jshint-file-reporter', {
+			filename: path.base + path.logs + 'jshint-output.log'
+		} ) );
 };
 
 function scss(){
@@ -128,12 +131,30 @@ function compressImg(){
 		.pipe( gulp.dest( path.base + path.cherryFramework.modules ) );
 };
 
+function watchHandler() {
+	var watcher = gulp.watch(
+		[
+			path.base + path.cherryFramework.modules + path.assets.js,
+			'!' + path.base + path.cherryFramework.modules + path.assets.ignorJs,
+			path.base + path.cherryFramework.modules + path.assets.scss,
+		],
+		task
+	);
+
+	watcher
+		.on( 'change', function( event ) {
+			console.log( 'Event type: ' + event.type );
+			console.log( 'Event path: ' + event.path );
+		} )
+		.on( 'error', function( event ) {
+			console.log( 'Event type: ' + event.type );
+			console.log( 'Event path: ' + event.path );
+		} );
+}
+
 function renameFile( path, subDir, sufix) {
 	path.dirname += subDir;
 	path.basename += sufix;
 
 	console.log( 'FILE: ' + __dirname + '/' + path.dirname + path.basename  + path.extname );
-}
-function logFiles( logFileName ){
-	logFileName = './logs/jshint.log';
-}
+};
